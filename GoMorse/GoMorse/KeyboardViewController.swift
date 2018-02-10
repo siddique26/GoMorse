0eebb074e0c8b167b012cbf03a6cc83821bedb7b
siddique26 <siddijafr@gmail.com>
2018-02-10 17:48:02 +0530

    Added UI Elements

---
+++ GoMorse/GoMorse/KeyboardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.swift
+//  GoMorse
+//
+//  Created by Siddique on 10/02/18.
+//  Copyright © 2018 Siddique. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+}
---
+++ GoMorse/GoMorse/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  GoMorse
+//
+//  Created by Siddique on 09/02/18.
+//  Copyright © 2018 Siddique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var morseLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBAction func deleteCode(_ sender: UIButton) {
+    }
+    @IBAction func clear(_ sender: UIButton) {
+    }
+    @IBAction func space(_ sender: Any) {
+    }
+    @IBAction func next(_ sender: UIButton) {
+    }
+    @IBAction func dash(_ sender: UIButton) {
+    }
+    @IBAction func dot(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
